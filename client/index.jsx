e9b6acbfc0b0ebf98c33037c67dcbdb53a51bf4c
Jaro Satkevic <jaro@nous.lt>
2016-03-24 19:05:01 +0000

    Redux

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,32 @@
-import React       from 'react';
-import { render }  from 'react-dom';
-import { Router }  from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-import routes      from 'routes';
+
+import React                            from 'react';
+import { Router }                       from 'react-router';
+import { render }                       from 'react-dom';
+import routes                           from 'routes';
+import { createStore, combineReducers } from 'redux';
+import { Provider }                     from 'react-redux';
+import * as reducers                    from 'reducers';
+import { fromJS }                       from 'immutable';
+import createBrowserHistory             from 'history/lib/createBrowserHistory';
 
 const history = createBrowserHistory();
 
+let initialState = window.__INITIAL_STATE__;
+
+// Transform into Immutable.js collections,
+// but leave top level keys untouched for Redux
+Object
+  .keys(initialState)
+  .forEach(key => {
+    initialState[key] = fromJS(initialState[key]);
+   });
+
+const reducer = combineReducers(reducers);
+const store   = createStore(reducer, initialState);
+
 render(
-  <Router children={routes} history={history} />,
+  <Provider store={store}>
+      <Router children={routes} history={history} />
+  </Provider>,
   document.getElementById('react-view')
 );
